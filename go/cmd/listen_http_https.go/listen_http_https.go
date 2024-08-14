8ece77c7b19d664c60717b0381e837981287d225
shawnyyu <shawnyyu@tencent.com>
2024-08-14 10:33:54 +0800

    listen_http_https

--- go/cmd/listen_http_https.go/listen_http_https.go
+++ go/cmd/listen_http_https.go/listen_http_https.go
@@ -3,42 +3,53 @@ package main
 import (
 	"bytes"
 	"crypto/tls"
-	"crypto/x509"
-	"encoding/json"
-	"flag"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"os"
 
-	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
 
-var (
-	port     int
-	caroots  string
-	keyfile  string
-	signcert string
-)
-
-func init() {
-	flag.IntVar(&port, "port", 8080, "The host port on which the REST server will listen")
-	flag.StringVar(&caroots, "caroot", "", "Path to file containing PEM-encoded trusted certificate(s) for clients")
-	flag.StringVar(&keyfile, "key", "", "Path to file containing PEM-encoded key file for service")
-	flag.StringVar(&signcert, "signcert", "", "Path to file containing PEM-encoded sign certificate for service")
+var upgrader = websocket.Upgrader{} // use default options
+func ws(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		_, _, err := c.ReadMessage()
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+	}
 }
 
-// Start a proxy server listen on listenport
-// This proxy will forward all HTTP request to httpport, and all HTTPS request to httpsport
-func proxyStart(listenport, httpport, httpsport int) {
-	proxylistener, err := net.Listen("tcp", fmt.Sprintf(":%d", listenport))
+func chunked(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Transfer-Encoding", "chunked")
+	for i := 0; i < 1024; i++ {
+		_, err := w.Write([]byte{0xFF})
+		if err != nil {
+			return
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws", ws)
+	http.HandleFunc("/chunked", chunked)
+
+	go HttpProxyServer()
+	go HttpsProxyServe()
+	proxylistener, err := net.Listen("tcp", ":58787")
 	if err != nil {
-		fmt.Printf("Unable to listen on: %d, error: %s\n", listenport, err.Error())
-		os.Exit(1)
+		panic(err)
 	}
 	defer proxylistener.Close()
-
 	for {
 		proxyconn, err := proxylistener.Accept()
 		if err != nil {
@@ -55,20 +66,18 @@ func proxyStart(listenport, httpport, httpsport int) {
 			continue
 		}
 
-		var targetport int
-		if isHTTPRequest(buffer) {
-			targetport = httpport
+		var targetport string
+		if IsHTTPRequest(buffer) {
+			targetport = http_unix_path
 		} else {
-			targetport = httpsport
+			targetport = https_unix_path
 		}
-
-		targetconn, err := net.Dial("tcp", fmt.Sprintf("localhost:%d", targetport))
+		targetconn, err := net.Dial("unix", targetport)
 		if err != nil {
 			fmt.Printf("Unable to connect to: %d, error: %s\n", targetport, err.Error())
 			proxyconn.Close()
 			continue
 		}
-
 		_, err = targetconn.Write(buffer[:n])
 		if err != nil {
 			fmt.Printf("Unable to write to output, error: %s\n", err.Error())
@@ -76,11 +85,21 @@ func proxyStart(listenport, httpport, httpsport int) {
 			targetconn.Close()
 			continue
 		}
-
 		go proxyRequest(proxyconn, targetconn)
 		go proxyRequest(targetconn, proxyconn)
 	}
 }
+func IsHTTPRequest(buffer []byte) bool {
+	httpMethod := []string{"GET", "PUT", "HEAD", "POST", "DELETE", "PATCH", "OPTIONS", "CONNECT", "TRACE"}
+	for cnt := 0; cnt < len(httpMethod); cnt++ {
+		if bytes.HasPrefix(buffer, []byte(httpMethod[cnt])) {
+			return true
+		}
+	}
+	return false
+}
+
+var http_unix_path = "/tmp/localproxy-server.http"
 
 // Forward all requests from r to w
 func proxyRequest(r net.Conn, w net.Conn) {
@@ -102,74 +121,103 @@ func proxyRequest(r net.Conn, w net.Conn) {
 		}
 	}
 }
-
-func isHTTPRequest(buffer []byte) bool {
-	httpMethod := []string{"GET", "PUT", "HEAD", "POST", "DELETE", "PATCH", "OPTIONS", "CONNECT", "TRACE"}
-	for cnt := 0; cnt < len(httpMethod); cnt++ {
-		if bytes.HasPrefix(buffer, []byte(httpMethod[cnt])) {
-			return true
-		}
-	}
-	return false
-}
-
-func startHTTPSServer(address string, router *mux.Router, caroots string, keyfile string, signcert string) {
-	pool := x509.NewCertPool()
-
-	caCrt, err := os.ReadFile(caroots)
+func HttpProxyServer() {
+	os.Remove(http_unix_path)
+	httpUnix, err := net.Listen("unix", http_unix_path)
 	if err != nil {
-		log.Fatalln("ReadFile err:", err)
+		panic(err)
 	}
-	pool.AppendCertsFromPEM(caCrt)
-
-	s := &http.Server{
-		Addr:    address,
-		Handler: router,
-		TLSConfig: &tls.Config{
-			MinVersion: tls.VersionTLS12,
-			ClientCAs:  pool,
-			ClientAuth: tls.RequireAndVerifyClientCert,
-		},
+	server := http.Server{
+		Handler: http.DefaultServeMux,
 	}
-	err = s.ListenAndServeTLS(signcert, keyfile)
+	err = server.Serve(httpUnix)
 	if err != nil {
-		log.Fatalln("ListenAndServeTLS err:", err)
+		panic(err)
 	}
+	defer func() {
+		os.Remove(http_unix_path)
+	}()
+
 }
 
-func startHTTPServer(address string, router *mux.Router) {
-	err := http.ListenAndServe(address, router)
+var https_unix_path = "/tmp/localproxy-server.https"
+
+func HttpsProxyServe() {
+	cert, err := tls.X509KeyPair([]byte(CERT), []byte(KEY))
 	if err != nil {
-		log.Fatalln("ListenAndServe err:", err)
+		panic(err)
+	}
+
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+	}
+	os.Remove(https_unix_path)
+	httpsUnix, err := tls.Listen("unix", https_unix_path, tlsConfig)
+	if err != nil {
+		panic(err)
+	}
+	server := http.Server{
+		Handler: http.DefaultServeMux,
+	}
+
+	err = server.Serve(httpsUnix)
+	if err != nil {
+		panic(err)
 	}
 }
 
-func SayHello(w http.ResponseWriter, r *http.Request) {
-	log.Println("Entry SayHello")
-	res := map[string]string{"hello": "world"}
-
-	b, err := json.Marshal(res)
-	if err == nil {
-		w.WriteHeader(http.StatusOK)
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(b)
-	}
-
-	log.Println("Exit SayHello")
-}
-
-func main() {
-	flag.Parse()
-	fmt.Println("Server listen on", port)
-
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/service/hello", SayHello).Methods("GET")
-
-	listenport, httpport, httpsport := port, port+10, port+20
-	go startHTTPServer(fmt.Sprintf("localhost:%d", httpport), router)
-	go startHTTPSServer(fmt.Sprintf("localhost:%d", httpsport), router, caroots, keyfile, signcert)
-
-	proxyStart(listenport, httpport, httpsport)
-
-	fmt.Println("Exit main")
-}
+var CERT = `-----BEGIN CERTIFICATE-----
+MIIEIjCCAoqgAwIBAgIRAJU8VQ/1Rja9KCLx8YIjWlkwDQYJKoZIhvcNAQELBQAw
+azEeMBwGA1UEChMVbWtjZXJ0IGRldmVsb3BtZW50IENBMSAwHgYDVQQLDBd4QFNI
+QVdOWVlVLU1CMCAo5L2Z5pifKTEnMCUGA1UEAwwebWtjZXJ0IHhAU0hBV05ZWVUt
+TUIwICjkvZnmmJ8pMB4XDTI0MDgxMjA4MDg1M1oXDTI2MTExMjA4MDg1M1owSzEn
+MCUGA1UEChMebWtjZXJ0IGRldmVsb3BtZW50IGNlcnRpZmljYXRlMSAwHgYDVQQL
+DBd4QFNIQVdOWVlVLU1CMCAo5L2Z5pifKTCCASIwDQYJKoZIhvcNAQEBBQADggEP
+ADCCAQoCggEBALLe9BdA2+SMSMPCGpCedeLg/voykZbbQ2fQ/LYPjeR3h0ITMxIt
+Qh3V9Rsyy3vvVnR8P8xhN/Dfdqg111ZXzouTZpfjQmPwOsezrGDGs+Bv3Wo23jzm
+NMNXEqRTA0OHr3q2nV0mc2HE9Ap165CZ42qlJ1VJXKzpoOanNQb6zGygG/WM3DNd
+NXSe/mSLpeft5l1BE1j7r+fOi3hViiHtyHeIsah20//GvbS9k4A1ojVE1hiaHJoQ
+b/RA3e7FRjMwbe0ZyL6JPaIFaFKQIndDP65c5joMjutFW4S4EJIdrViF6hZg2Ug4
+ICdVU+hleLL/dimLy4phr2gh+zj0bt5YVSUCAwEAAaNhMF8wDgYDVR0PAQH/BAQD
+AgWgMBMGA1UdJQQMMAoGCCsGAQUFBwMBMB8GA1UdIwQYMBaAFIg5tCJhKyWy9YR/
+3NrgEraBY76VMBcGA1UdEQQQMA6CDHh4Lnl4aW5nLnh5ejANBgkqhkiG9w0BAQsF
+AAOCAYEAVVithC9KiAqhuE8eCrfURzmQrMAARuifJUxkuLVNZ+ce8rjo++l1a7Ox
+lv5WlylLJXsbAKvFclNiE2FUydz47TS/M4+AIxhzyE1bVvbY8zhx75oEyqpJGdBd
+bgO7JDJRc3YbvqOA25sSJCop5wYXBuqlLvivloBQJCJqNvI04QnuU9WG3m4gZ/+C
+P6lvqqBiqfrtYzIq7qRqjC9OCnXIUgaJ01me2hxV/pcwLWCq6LUp2aEpQ1KaW7xC
+OsrnjvW9t2f0PflKeOfw0bjS0JxWHK4YYGYMOiSeADWpB1ERIU7zU13jDVIahKO+
+ekeOf2VhdsChAjpLUWHyiqgG6VmJkXjfAcg/6EwjxdqXKLuUIDkQ6oxlBOYsDTac
+NNAJyVF95d9n5l1cHsl3uqiUHgnZdCVp/sVefnx5mL2Yfq6c49bgUOIWPOXRtuvO
+StAeaKLbALJsufOgB3DwiC9vBsBnfj9oNCPxUdeCzSJnH3U6/YLi2bLcXwq5uZXQ
+9S9fgSv+
+-----END CERTIFICATE-----
+`
+var KEY = `-----BEGIN PRIVATE KEY-----
+MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCy3vQXQNvkjEjD
+whqQnnXi4P76MpGW20Nn0Py2D43kd4dCEzMSLUId1fUbMst771Z0fD/MYTfw33ao
+NddWV86Lk2aX40Jj8DrHs6xgxrPgb91qNt485jTDVxKkUwNDh696tp1dJnNhxPQK
+deuQmeNqpSdVSVys6aDmpzUG+sxsoBv1jNwzXTV0nv5ki6Xn7eZdQRNY+6/nzot4
+VYoh7ch3iLGodtP/xr20vZOANaI1RNYYmhyaEG/0QN3uxUYzMG3tGci+iT2iBWhS
+kCJ3Qz+uXOY6DI7rRVuEuBCSHa1YheoWYNlIOCAnVVPoZXiy/3Ypi8uKYa9oIfs4
+9G7eWFUlAgMBAAECggEAKB4/avO+Hg2kyFtBsAwKbr9+EMnEw6wb+y3xcDge4A47
+BQPfYYVLHfv+BIKpBvwQTQINIR7w+BJ2v5WL3a7GAaIm0YxEOLwJwv62e+I1N/J/
+G7KAt/H+BY4C/V4tDjlhj0lkFB9qo5QUFECMfBs32ZR/NO7GXXXtA66fAxi/PuFu
+FrfK/Cuz3lXn8QIhpmksqO7dpz/c6XknU6qfF6cepVVSlfhIw8NqmxNLg9EAsIH7
+Od2FjrMxZkQKbFkozAU/b0x3tsg/PwAs83HzSf/zUcX9AP9OOBhxcoNtox4G89Bi
+jzD/hIZPOv7CcQSrj7pySXduKEg9mYuo7EZRv4oChQKBgQDjvUf7RAp4VAbN3VNd
+Nx+hAe5CTrd6mhdjs48rXsW24BKmewGL0zvYpRPa0Wim3z0fMLhbqneKd3GfTpE8
+cqcGMvhIHEkfGMBAD2LqmUY7DXWlYaUWAEJyXChOz118Q3/Ic9AdNAggaggKfiZs
+UaVSp43KmdkwZhmprvbSnf4/ywKBgQDJET3UjXegIPbS+FqZhufRtiylxsCgWtQj
+qVzeCOeN2RXxWCW6xXzwIRkoNsJ0ICZ53WbXLChViPnMNlrtAycZumBbbbTk4mqE
+COLxZvrIg0Xp+UxDuNvWPslm1sSLZ7FKcszyh7HFX2TpsyGElGGcJqZAD4tfMT4/
+iChiT3BAzwKBgDwbS/E8Lws9GiKhZIw4rUdgbBLiFbjtDHlK/eFzfPlcQG/iDTFr
+SeNPBmN9W4KXbtlZkX7YCf7osXtbUCfFFuIi97aIiKAFd1Aw/2ltlMSnM8K3d8vL
+u73VJupN/p16bzJnpqjef7qWYZLrYpa6Ickj4d90JYeJmwJW5FwISP9rAoGAaEG2
+a8ZG5xLwqQf6Am1/OrBZikP6katHII6rBr5bQqPzysbIGYZZeDHRV5a9UXVyPfJd
+ZukQqPlzfT4Z+5eM6LxJRl8mUyBL1ta8xit9kgbvc5i+wMbTxs2bpOVr2FUWCuJn
+/sH5nbwPGVa25IYD7vHjdogY3m0sN8kkF4XVUOUCgYB2L0TC51Gzk2UzqPHmJgTL
+X2KVGcMpzUsifporSt+kg1Gl+JG6b/VSbGwdenFl+zy4pACFaz9HnvoX224tW3HN
+fE4sV7VGMuKxTabj8GpBBmemUVOLZRVrtMClHRiWg8NTwYHA7koVFD6ml/T+kv+p
+ASpCs86w4R3crQSD2CB1CQ==
+-----END PRIVATE KEY-----
+`
--- go/go.mod
+++ go/go.mod
@@ -1,18 +1,44 @@
 module code
 
-go 1.20
+go 1.21
+
+toolchain go1.22.6
 
 require (
-	github.com/gorilla/mux v1.8.0
+	github.com/docker/docker v27.1.2+incompatible
+	github.com/gorilla/websocket v1.5.3
+	github.com/moby/moby v27.1.2+incompatible
 	github.com/pkg/errors v0.9.1
 	github.com/shirou/gopsutil v3.21.11+incompatible
 	github.com/vishvananda/netlink v1.1.0
-	golang.org/x/net v0.15.0
+	golang.org/x/crypto v0.24.0
 )
 
 require (
+	github.com/Microsoft/go-winio v0.4.14 // indirect
+	github.com/containerd/log v0.1.0 // indirect
+	github.com/distribution/reference v0.6.0 // indirect
+	github.com/docker/go-connections v0.5.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
+	github.com/moby/term v0.5.0 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0 // indirect
 	github.com/vishvananda/netns v0.0.0-20191106174202-0a2b9b5464df // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
-	golang.org/x/sys v0.12.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.53.0 // indirect
+	go.opentelemetry.io/otel v1.28.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.28.0 // indirect
+	go.opentelemetry.io/otel/metric v1.28.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.28.0 // indirect
+	go.opentelemetry.io/otel/trace v1.28.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/time v0.6.0 // indirect
+	gotest.tools/v3 v3.5.1 // indirect
 )
